94113c35178318f0102616ae17735d1fdf89e193
test <inheuk1117@naver.com>
2024-10-22 23:12:57 +0900

    random next

--- SAP_FI_FLASK.py
+++ SAP_FI_FLASK.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
 import random
-from time import localtime,time 
 
 app = Flask(__name__)
 
@@ -18,18 +17,14 @@ quiz_data = load_quiz_data()
 
 @app.route("/", methods=["GET", "POST"])
 def quiz():
-    
 
     # 질문의 순서를 무작위로 섞기
-    tm = localtime(time())
-    random.seed(tm.tm_sec)
-    random.shuffle(quiz_data)
-    # URL에서 question_index 가져오기, 기본값은 0
+    quiz_data = random.shuffle(quiz_data)
+
     question_index = int(request.args.get("question_index", 0))
     result = request.args.get("result", "")
 
     if request.method == "POST":
-        # 사용자가 제출한 답변 가져오기
         user_answers = request.form.getlist("answer")
         correct_answers = quiz_data[question_index]['infoAnswer']
 
@@ -39,20 +34,18 @@ def quiz():
         else:
             result = f"오답입니다! 정답은 {', '.join(correct_answers)}입니다."
 
-        # 리다이렉트로 결과 전달
         return redirect(url_for('quiz', question_index=question_index, result=result))
 
-    # 다음 질문으로 넘어가는 경우 준비
     choices = [(chr(65 + i), choice) for i, choice in enumerate(quiz_data[question_index]['infoChoice'])]
 
-    # 템플릿 렌더링
     return render_template(
         "quiz.html",
         choices=choices,
         question=quiz_data[question_index],
         question_index=question_index,
         result=result,
-        quiz_length=len(quiz_data)  # 총 질문 수 전달
+        quiz_length=len(quiz_data)
     )
+
 if __name__ == '__main__':
     app.run()
